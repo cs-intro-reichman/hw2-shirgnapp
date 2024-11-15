f3c83538587d621bccbbdcd522ba7c0e80f23bfc
shirgnapp <gnapp3@gmail.com>
2024-11-15 13:38:36 +0200

    cheaked

--- Cheers.java
+++ Cheers.java
@@ -4,18 +4,18 @@ public class Cheers {
         String cheers=args[0];
         cheers=cheers.toUpperCase();
         int times= Integer.parseInt(args[1]);
-        int leneth=cheers.length();
+        int length=cheers.length();
         char a;
         int i = 0;
-        while (i<leneth) {
+        while (i<length) {
                 a= cheers.charAt(i);
                 if(a=='A' || a=='E' || a== 'F' || a== 'H' || a== 'I' || a== 'L' || a== 'M' || a== 'N' || a== 'O' || a== 'R' ||  a== 'S' || a=='X')
                 {
-                        System.out.println("Give me an " + a + ": " + a + "!");
+                        System.out.println("Give me an  " + a + ": " + a + "!");
 
                 }
                 else
-                System.out.println("Give me a " + a + ": " + a + "!");
+                System.out.println("Give me a  " + a + ": " + a + "!");
                 i++;
         }
         System.out.println("What does that spell?");

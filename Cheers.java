254207efc37eb682da8038c0206dd4cdce6be0b1
shirgnapp <gnapp3@gmail.com>
2024-11-21 09:55:21 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,32 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-        String cheers=args[0];
-        cheers=cheers.toUpperCase();
-        int times= Integer.parseInt(args[1]);
-        int length=cheers.length();
+        String cheers = args[0];
+        cheers = cheers.toUpperCase();
+        int times = Integer.parseInt(args[1]);
+        int length = cheers.length();
         char a;
         int i = 0;
-        while (i<length) {
-                a= cheers.charAt(i);
-                if(a=='A' || a=='E' || a== 'F' || a== 'H' || a== 'I' || a== 'L' || a== 'M' || a== 'N' || a== 'O' || a== 'R' ||  a== 'S' || a=='X')
+        while (i < length) {
+                a = cheers.charAt(i);
+                if( a=='A' || a=='E' || a== 'F' || a== 'H' || a== 'I' || a== 'L' || a== 'M' || a== 'N' || a== 'O' || a== 'R' ||  a== 'S' || a=='X' )
                 {
-                        System.out.println("Give me an " + a + ": " + a + "!");
+                        System.out.println( "Give me an " + a + ": " + a + "!" );
 
                 }
                 else
-                System.out.println("Give me a  " + a + ": " + a + "!");
+                System.out.println( "Give me a  " + a + ": " + a + "!" );
                 i++;
         }
         System.out.println("What does that spell?");
         for(int j =0; j < times; j++)
         {
-                System.out.println(cheers + "!!!");
+                System.out.println( cheers + "!!!" );
         }
-        
-        
-               
-        
-
 }
 }

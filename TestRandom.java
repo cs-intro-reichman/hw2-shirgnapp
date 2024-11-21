f975a71350eee16a916449be9ce44a0eb290f453
shirgnapp <gnapp3@gmail.com>
2024-11-21 09:53:06 +0200

    done

--- TestRandom.java
+++ TestRandom.java
@@ -2,11 +2,11 @@
 public  class  TestRandom {
 	public static void main(String[]  args) {
 	    int times= Integer.parseInt(args[0]);
-		int i=1, countSmall=0, countBig=0;
-		double random=0, ratio=0;
-		while (i<=times) {
-			random= Math.random();
-			ratio+=random;
+		int i = 1, countSmall = 0, countBig = 0;
+		double random = 0, ratio = 0;
+		while ( i<=times ) {
+			random = Math.random();
+			ratio += random;
 			if (random<0.5) {
 				countSmall++;
 			}
@@ -14,11 +14,9 @@ public  class  TestRandom {
 			countBig++;
 			i++;
 		}
-		ratio/=times;
+		ratio /= times;
 		System.out.println("> 0.5 :" + countBig);
 		System.out.println("< 0.5: " + countSmall);
 		System.out.println( " Ratio: " + ratio);
-
-
 	}
 }
